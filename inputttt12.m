1664ba9b084979a00597dd6621c81eaf1f404689
Varsha Patil <73213752+VarshaP29@users.noreply.github.com>
2021-07-10 19:28:20 +0530

    Add files via upload

---
+++ inputttt12.m
@@ -0,0 +1,36 @@
+
+Acceleration1=timeseries;
+Acceleration1.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
+Acceleration1.data=xlsread('CRDI_MIL.xlsx','C2:C3059');
+
+Torque1=timeseries;
+Torque1.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
+Torque1.data=xlsread('CRDI_MIL.xlsx','D2:D3059');
+
+Water_in_Fuel=timeseries;
+Water_in_Fuel.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
+Water_in_Fuel.data=xlsread('CRDI_MIL.xlsx','E2:E3059');
+
+Up_stream_partical1=timeseries;
+Up_stream_partical1.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
+Up_stream_partical1.data=xlsread('CRDI_MIL.xlsx','F2:F3059');
+
+%Injector 1 Expected Value
+Down_stream_partical1=timeseries;
+Down_stream_partical1.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
+Down_stream_partical1.data=xlsread('CRDI_MIL.xlsx','G2:G3059');
+
+%Injector 2 Expected Value
+LPP_Pressue1=timeseries;
+LPP_Pressue1.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
+LPP_Pressue1.data=xlsread('CRDI_MIL.xlsx','I2:I3059');
+
+%Injector 3 Expected Value
+Fuel_Percentage11=timeseries;
+Fuel_Percentage11.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
+Fuel_Percentage11.data=xlsread('CRDI_MIL.xlsx','H2:H3059');
+
+%Injector 4 Expected Value
+Rail_Pressure_Sensor1=timeseries;
+Rail_Pressure_Sensor1.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
+Rail_Pressure_Sensor1.data=xlsread('CRDI_MIL.xlsx','J2:J3059');

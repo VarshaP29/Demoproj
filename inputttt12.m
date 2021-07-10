72aa4ea5e5a6b6b999b4038e9c65d3409239d0b3
Varsha Patil <73213752+VarshaP29@users.noreply.github.com>
2021-07-10 20:22:40 +0530

    Deleteing Script

--- inputttt12.m
+++ inputttt12.m
@@ -1,12 +1,3 @@
-
-Acceleration1=timeseries;
-Acceleration1.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
-Acceleration1.data=xlsread('CRDI_MIL.xlsx','C2:C3059');
-
-Torque1=timeseries;
-Torque1.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
-Torque1.data=xlsread('CRDI_MIL.xlsx','D2:D3059');
-
 Water_in_Fuel=timeseries;
 Water_in_Fuel.time=xlsread('CRDI_MIL.xlsx','A2:A3059');
 Water_in_Fuel.data=xlsread('CRDI_MIL.xlsx','E2:E3059');
